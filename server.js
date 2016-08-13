48b9c112ae6dc7d9599972081e1d93ae158c043d
Seth Cohen <forwardthinking.llc@gmail.com>
2016-08-13 17:33:53 -0400

    Update server to use https

--- server.js
+++ server.js
@@ -3,11 +3,15 @@
  */
 var AlexaAppServer = require('alexa-app-server');
 var server = new AlexaAppServer({
-  server_root:__dirname,     // Path to root
-  public_html:"public_html", // Static content
-  app_dir:"apps",            // Where alexa-app modules are stored
-  app_root:"/alexa/",        // Service root
-  port:8080                  // What port to use, duh
+  server_root: __dirname,     // Path to root
+  public_html: 'public_html', // Static content
+  app_dir: 'apps',            // Where alexa-app modules are stored
+  app_root: '/alexa/',        // Service root
+  port: 8080,                 // What port to use, duh
+  httpsPort : 443,
+  httpsEnabled: true,
+  privateKey: 'private-key.pem',
+  certificate: 'cert.cer'
 });
 server.start();
 server.express.use('/test',function(req,res){ res.send("OK"); });
